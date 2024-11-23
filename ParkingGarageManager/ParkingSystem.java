a4e2e2699ebcf945b326a2ea5aafdebeddcf933a
jjfishtank <26157427+jjfishtank@users.noreply.github.com>
2024-11-22 19:31:07 -0800

    hourlyRate and calcFees(Customer)

--- ParkingGarageManager/ParkingSystem.java
+++ ParkingGarageManager/ParkingSystem.java
@@ -1,4 +1,21 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class ParkingSystem {
+	float hourlyRate;
 	
+	public ParkingSystem(float hourlyRate) {
+		this.hourlyRate = hourlyRate;
+	}
+	
+	/**
+	 * Gets the timeIn from Customer attribute and returns a fee based on 
+	 * hourly rate and time stayed from timeIn to now.
+	 * */
+	public float calcFees(Customer customer) {
+		LocalDateTime timeIn = customer.getTimeIn();
+		Duration timeParked = Duration.between(timeIn, LocalDateTime.now());
+		long hoursParked = timeParked.toHours();
+		return hoursParked * hourlyRate;
+	}
 }
