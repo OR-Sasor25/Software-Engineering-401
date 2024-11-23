f9f7a2be5a25f889ee245e28fddc2e7b83432a91
jjfishtank <26157427+jjfishtank@users.noreply.github.com>
2024-11-22 18:45:22 -0800

    Implement attributes and park/remove Vehicle methods

--- ParkingGarageManager/Garage.java
+++ ParkingGarageManager/Garage.java
@@ -1,4 +1,45 @@
+import java.util.UUID;
 
 public class Garage {
-
+	private final String garageID;
+	private int capacity;
+	private int spacesTaken;
+	
+	public Garage(int capacity) {
+		garageID = UUID.randomUUID().toString();
+		this.capacity = capacity;
+		spacesTaken = 0;
+	}
+	
+	public boolean parkVehicle() {
+		if (checkSpace()) {
+			spacesTaken++;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean removeVehicle() {
+		if (spacesTaken > 0) {
+			spacesTaken--;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean checkSpace() {
+		return spacesTaken < capacity;
+	}
+	
+	public String getID() {
+		return garageID;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public int getSpacesTaken() {
+		return spacesTaken;
+	}
 }
