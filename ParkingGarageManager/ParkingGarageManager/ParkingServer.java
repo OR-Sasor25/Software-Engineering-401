279083726fa6bd3ae4089d9851b910d82e0bde22
pixleboi69 <47615951+OR-Sasor25@users.noreply.github.com>
2024-11-27 13:45:47 -0800

    Full Server Skeleton
    A server that takes request from a client as an int. methods for modifying garage objects to be implemented

--- ParkingGarageManager/ParkingGarageManager/ParkingServer.java
+++ ParkingGarageManager/ParkingGarageManager/ParkingServer.java
@@ -12,7 +12,9 @@ import java.util.concurrent.Executors;
 
 
 public class ParkingServer {
-	
+		//hard coded employee credentials
+	    static String managerUsername = "manager";
+        static String managerPassword = "password";
 	
 	public static void main(String[] args) throws Exception{
 		try (var listener = new ServerSocket(59898)) {
@@ -38,17 +40,60 @@ public class ParkingServer {
 				//Input and Output streams to allow passing objects between server and client
 				ObjectOutputStream oOStream = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream oIStream = new ObjectInputStream(socket.getInputStream());
+				InputStream request = (socket.getInputStream());
 				
+				//variable that reads ManagerLogInObjects
+				ManagerLogIn employee = (ManagerLogIn)oIStream.readObject();
+				
+				//if the attributes of employee match managerUsername and managerPassword allow the employee to modify a garage
+				if(employee.getId().equals(managerUsername) && employee.getPassword().equals(managerPassword)) {
+					//request are passed to choice as an int
+					int choice;
+					do {
+						choice = request.read();
+						switch(choice) {
+						case 0: doAddTicket();
+						case 1: doPayTicket();
+						case 2:	doWriteReport();
+						case 3: doRetrieveTicket();
+						case 4: System.out.print("Client is loging out");
+						default: System.out.print("INVALID REQUEST");
+							break;
+						
+						}
+					}while(choice != 4);
+				}else {
+					System.out.print("Invalid Username or Password");
+				}
 				
 			} catch (IOException e) {
 				System.out.println("Error:" + socket);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			} finally {
 				try {
                     socket.close();
                 } catch (IOException e) {
                 }
-                System.out.println("Closed: " + socket);
+                System.out.println("Conection from Garage Terminated");
             }
+		}
+		//methods for passing objects into server and modifying garages
+		private void doRetrieveTicket() {
+			// TODO Auto-generated method stub
+			
+		}
+		private void doAddTicket() {
+			// TODO Auto-generated method stub
+			
+		}
+		private void doPayTicket() {
+			// TODO Auto-generated method stub
+			
+		}
+		private void doWriteReport() {
+			// TODO Auto-generated method stub
+			
 		}
 	}
 }
