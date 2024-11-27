15e9359fd0076af8cda252adefc7439194554a78
Timofay Udalkin <125611735+Tudalkin@users.noreply.github.com>
2024-11-26 22:01:20 -0800

    comment with items needed from server and back

--- ParkingGarageManager/ParkingGarageManager/ParkingClient.java
+++ ParkingGarageManager/ParkingGarageManager/ParkingClient.java
@@ -1,5 +1,17 @@
 package ParkingGarageManager;
-
+/*need Items to be passed here:
+ * Manager:
+ *	 	manager login and password to be sent to server validation to be sent back
+ * 		report for manager
+ * 
+ * Customer:
+ * 		is garage full?
+ * 		ticket with unique id, timestamp, and unpaidstatus sent to client
+ * 
+ * 		ticket with paid status and amount for report processing sent to server
+ * 		
+ * 
+ * */
 public class ParkingClient {
     public static void main(String[] args) {
     	ManagerGUI mgui = new ManagerGUI();
