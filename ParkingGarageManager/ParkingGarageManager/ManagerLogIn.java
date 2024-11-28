16dafc86864437917e30ce344e66ec0e26c9067f
pixleboi69 <47615951+OR-Sasor25@users.noreply.github.com>
2024-11-27 16:52:24 -0800

    Update ManagerLogIn.java
    now implements serializable to be passed to the server

--- ParkingGarageManager/ParkingGarageManager/ManagerLogIn.java
+++ ParkingGarageManager/ParkingGarageManager/ManagerLogIn.java
@@ -1,5 +1,8 @@
 package ParkingGarageManager;
-public class ManagerLogIn {
+
+import java.io.Serializable;
+
+public class ManagerLogIn implements Serializable{
     private String id;
     private String password;
 
