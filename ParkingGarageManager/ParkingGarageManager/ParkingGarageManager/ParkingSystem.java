066a759d121d9aa97a14e7f81f3781fc4e522578
jjfishtank <26157427+jjfishtank@users.noreply.github.com>
2024-12-01 16:22:58 -0800

    Update ParkingSystem.java
    Use ticket instead of customer

--- ParkingGarageManager/ParkingGarageManager/ParkingGarageManager/ParkingSystem.java
+++ ParkingGarageManager/ParkingGarageManager/ParkingGarageManager/ParkingSystem.java
@@ -1,9 +1,14 @@
 package ParkingGarageManager;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ParkingSystem {
 	float hourlyRate;
+	
+	public ParkingSystem() {
+		this.hourlyRate = 10.00;
+	}
 	
 	public ParkingSystem(float hourlyRate) {
 		this.hourlyRate = hourlyRate;
@@ -13,8 +18,8 @@ public class ParkingSystem {
 	 * Gets the timeIn from Customer attribute and returns a fee based on 
 	 * hourly rate and time stayed from timeIn to now.
 	 * */
-	public float calcFees(Customer customer) {
-		LocalDateTime timeIn = customer.getTimeIn();
+	public float calcFees(Ticket ticket) {
+		LocalDateTime timeIn = LocalDateTime.parse(ticket.getTimeArrived(), DateTimeFormatter.ISO_DATE_TIME);
 		Duration timeParked = Duration.between(timeIn, LocalDateTime.now());
 		long hoursParked = timeParked.toHours();
 		return hoursParked * hourlyRate;
